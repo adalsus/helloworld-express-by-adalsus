6cde380a0b41938b75f53dcccba273594ea7c3ad
Glitch (hello-express) <none>
2023-06-24 06:52:15 +0000

    👚🚲 Checkpoint ./server.js:57861315/134 ./package.json:57861315/1265

--- server.js
+++ server.js
@@ -12,15 +12,16 @@ const app = express();
 app.use(express.static("public"));
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function(request, response) {
+/*app.get("/", function(request, response) {
   response.sendFile(__dirname + "/views/index.html");
-});
+});*/
 
-app.get("/adalsus", (req, res) => {
-  res.send('Hola Adalberto!!!');
+app.get("/", (req, res) => {
+  res.send('Hello World!!!');
 });
 
 // listen for requests :)
-const listener = app.listen(process.env.PORT, function() {
-  console.log("Your app is listening on port" + listener.address().port);
+const listener = app.listen(process.env.PORT, function(error) {
+  if (!error) console.log("Your app is listening on port " + listener.address().port)
+  else console.log(error.message);
 });
