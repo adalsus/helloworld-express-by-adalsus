fd9960c4882ac28d92bcf8d2b3e6cfd7d7ffee75
Glitch (hello-express) <none>
2023-06-28 00:43:26 -0500

    creo archivo .env para usar constantes de ese archivo en la app

--- server.js
+++ server.js
@@ -1,27 +1,10 @@
-// server.js
-// where your node app starts
-
 // init project
-const express = require("express");
-const app = express();
-
-// we've started you off with Express,
-// but feel free to use whatever libs or frameworks you'd like through `package.json`.
-
-// http://expressjs.com/en/starter/static-files.html
-app.use(express.static("public"));
-
-// http://expressjs.com/en/starter/basic-routing.html
-/*app.get("/", function(request, response) {
-  response.sendFile(__dirname + "/views/index.html");
-});*/
-
-app.get("/", (req, res) => {
-  res.send('Hello World!!!');
-});
+const server = require('./app.js');
+require('dotenv').config();
+const PORT = process.env.PORT || 3001;
 
 // listen for requests :)
-const listener = app.listen(process.env.PORT || 3001, function(error) {
+const listener = server.listen(PORT, function(error) {
   if (!error) console.log("Your app is listening on port " + listener.address().port)
   else console.log(error.message);
 });
